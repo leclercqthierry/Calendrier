678a6b42cbc0191b56840367caf7a163019be873
Thierry Leclercq <leclercqthierry80@gmail.com>
2025-02-05 11:46:10 +0100

    bug with arrows fixed

--- assets/js/navigate.js
+++ assets/js/navigate.js
@@ -4,30 +4,36 @@ const divs = document.querySelectorAll('div');
 const leftArrow = document.getElementById('leftArrow');
 const rightArrow = document.getElementById('rightArrow');
 
+// on récupère l'année courante
+let currentYear = new Date().getFullYear();
+
+// on va afficher (ou pas) les flèches selon le mois et l'année sélectionnée avant navigation
+let currentH2 = document.querySelector('div.show h2');
+
+
+// Si le h2 affiché est celui du tout premier mois disponible à l'affichage initial du calendrier on n'affiche pas la flèche gauche sinon on l'affiche
+leftArrow.style.display = (currentH2.textContent === `Calendrier de Janvier ${currentYear-5}`) ? 'none' : 'block';
+console.log(leftArrow.style.display);
+
+// Si le h2 affiché est celui du tout dernier mois disponible à l'affichage initial du calendrier on n'affiche pas la flèche gauche sinon on l'affiche
+rightArrow.style.display = (currentH2.textContent === `Calendrier de Décembre ${currentYear+5}`)? 'none' : 'block';
+
 // la fonction montre le mois suivant
 function showNextMonth(){
     let current = 0;
 
-    // on parcourt toutes les div et cherche celle avec la classe show
+    // on récupère l'index du mois affiché
     for (let i = 0; i < divs.length; i++) {
         if (divs[i].classList.contains('show')) {
-            // on récupère son index
             current = i;
 
-            // on ajoute la classe hidden
+            // 
             divs[i].classList.add('hide');
-
-            // puis retire la classe show
             divs[i].classList.remove('show');
-            // on break pour ne pas continuer à chercher une div avec la classe show vu qu'il n'y en a qu'une à la fois qui la possède
             break;
         }
     }
 
-    // si on est au dernier mois, on masque la flèche de droite
-    if (current == divs.length - 1) {
-        rightArrow.style.display = 'none';
-    }
     // On attends 300 millisecondes pour que l'image ne disparaisse pas instantanément (j'utilise donc une fonction anonyme vide vu qu'on ne fait rien d'autre qu'attendre 300 millisecondes)
     setTimeout(()=>{},300);
 
@@ -36,6 +42,11 @@ function showNextMonth(){
 
     // On retire la classe hide à la div suivante
     divs[current+1].classList.remove('hide');
+
+    // On récupère le nouveau h2 affiché
+    currentH2 = document.querySelector('div.show h2');
+    rightArrow.style.display = (currentH2.textContent === `Calendrier de Décembre ${currentYear+5}`)? 'none' : 'block';
+    leftArrow.style.display = 'block';
 }
 
 rightArrow.addEventListener('click',showNextMonth);
@@ -53,13 +64,12 @@ function showPrevMonth(){
         }
     }
 
-    // Cette fois ci on arrive au premier mois du calendrier
-    if (current == 0) {
-        leftArrow.style.display = 'none';
-    }
     setTimeout(()=>{},300);
     divs[current-1].classList.add('show');
     divs[current-1].classList.remove('hide');
+    currentH2 = document.querySelector('div.show h2');
+    leftArrow.style.display = (currentH2.textContent === `Calendrier de Janvier ${currentYear-5}`) ? 'none' : 'block';
+    rightArrow.style.display = 'block';
 }
 
 leftArrow.addEventListener('click',showPrevMonth);
